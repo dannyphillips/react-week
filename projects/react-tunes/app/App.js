1ea76672789e58c8dfc471987ceeeb7fc727cb74
Danny Phillips <dannyp@procore.com>
2015-03-11 15:44:51 -0600

    fixed apps from day 2 and removed npm from global

--- projects/react-tunes/app/App.js
+++ projects/react-tunes/app/App.js
@@ -2,8 +2,38 @@ var React = require('react');
 var SearchItunes = require('./SearchItunes');
 var Griddle = require('griddle-react');
 
-var App = React.createClass({
+var ImageComponent = React.createClass({
+  render: function() {
+    return (
+      <img src={this.props.data} />
+    );
+  }
+});
+
+var UrlComponent = React.createClass({
   render: function(){
+    return (
+      <a href={this.props.data}> 
+        {this.props.rowData.trackName}
+      </a>
+    );
+  }
+});
+
+var App = React.createClass({
+  getInitialState: function(){
+    console.log('getting getInitalState');
+    return {
+      data: ''     
+    };
+  },
+  updateState: function(info) {
+    console.log(info);
+    this.setState({data: info});
+  },
+  render: function(){
+    var heading = this.state.data === '' ? 'Make a Search' : 'Your Search Results';
+    console.log(heading);
     var griddleMeta = [
       {columnName: 'trackName',displayName: 'Name'},
       {columnName: 'artistName',displayName: 'Artist'},
@@ -12,25 +42,40 @@ var App = React.createClass({
       {columnName: 'trackPrice',displayName: 'Price'},{columnName: 'kind',displayName: 'Type'},
       {columnName: 'trackViewUrl',displayName: 'Online Link',customComponent: UrlComponent}
     ];
+
+    var styles = {
+      cushion: {
+        margin: 7
+      },
+      topCushion: {
+        marginTop: 49,
+        borderRadius: 0
+      }
+    };
     return (
       <span>
         <div className="navbar navbar-default navbar-fixed-top" role="navigation">
           <div className="container">
             <div className="row">
-              <div className="col-sm-6">
-                /* SearchItunes goes here passing it updateState as cb */
+              <div className="col-sm-6" style={styles.cushion}>
+                <SearchItunes cb={this.updateState} />
               </div>
             </div>
           </div>
         </div>
-        <div className="panel panel-default" >
+        <div className="panel panel-default" style={styles.topCushion} >
           <div className="panel-heading">
-            /* Have this say "Make a Search" when the "data" state is an empty string and have it change to "Your Search Results" when it's not */
+            {heading}
           </div>
-          /* Griddle Component Goes Here */
+            <Griddle
+              results={this.state.data}
+              tableClassName="table"
+              columnMetadata={griddleMeta}
+              columns={["trackName", "artistName", "primaryGenreName", "artworkUrl100", "trackPrice", "kind", "trackViewUrl"]}
+              />
         </div>
       </span>
-    )
+    );
   }
 });
 
--- projects/react-tunes/app/SearchItunes.js
+++ projects/react-tunes/app/SearchItunes.js
@@ -2,18 +2,44 @@ var React = require('react');
 var $ = require('jquery');
 
 var SearchItunes = React.createClass({
+  
+  propTypes: {
+    cb: React.PropTypes.func.isRequired
+  },
+  formatUrl: function(){
+    var searchTerm = this.refs.search.getDOMNode().value;
+    var entity = this.refs.entity.getDOMNode().value;
+    return "https://itunes.apple.com/search?term=" + searchTerm + "&entity=" + entity;
+  },
+  handleSubmit: function(){
+    var url = this.formatUrl();
+    console.log(url);
+    $.ajax({
+      url: url,
+      dataType: 'JSONP',
+      error: function(error){
+        console.log("Error: ", error);
+      },
+      success: function(data){
+        this.props.cb(data.results);
+      }.bind(this)
+    });
+  },
   render: function(){
     return (
       <div className="row">
         <div className="col-sm-12">
           <div className="input-group-inline col-sm-4">
-            /* 'search' input field goes here */
+            <input type="text" ref='search' /> 
           </div>
           <div className="input-group-inline col-sm-4">
-            /* 'entity' select tag goes here */
+            <select className="form-control" ref="entity">
+              <option value="musicTrack">Music</option>
+              <option value="movie">Movies</option>
+            </select>
           </div>
           <div className="input-group-inline col-sm-4">
-            /*handleSubmit button goes here*/
+            <button className="btn btn-primary" onClick={this.handleSubmit}>Submit</button>
           </div>
         </div>
       </div>
